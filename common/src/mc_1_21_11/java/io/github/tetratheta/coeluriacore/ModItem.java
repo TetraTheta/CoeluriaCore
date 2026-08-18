9dfb2e081ac375c30b3e03b9c818bf9597437dc9
TetraTheta <37061801+TetraTheta@users.noreply.github.com>
2026-08-18 17:56:21 +0900

    wip: 여러 Minecraft 버전 지원

--- common/src/mc_1_21_11/java/io/github/tetratheta/coeluriacore/ModItem.java
+++ common/src/mc_1_21_11/java/io/github/tetratheta/coeluriacore/ModItem.java
@@ -3,8 +3,8 @@ package io.github.tetratheta.coeluriacore;
 import dev.architectury.registry.registries.DeferredRegister;
 import dev.architectury.registry.registries.RegistrySupplier;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 
 @SuppressWarnings("UnstableApiUsage")
@@ -14,11 +14,9 @@ public class ModItem {
   public static RegistrySupplier<Item> NETHERITE_SCRAP_SHARD;
 
   static {
-    // Netherite Scrap Shard
-    ResourceLocation idNetheriteScrapShard = ResourceLocation.fromNamespaceAndPath("coeluriacore", "netherite_scrap_shard");
+    Identifier idNetheriteScrapShard = Identifier.fromNamespaceAndPath("coeluriacore", "netherite_scrap_shard");
     ResourceKey<Item> keyNetheriteScrapShard = ResourceKey.create(Registries.ITEM, idNetheriteScrapShard);
 
-    // Register things
     NETHERITE_SCRAP_SHARD = ITEMS.register(idNetheriteScrapShard, () -> new Item(new Item.Properties().setId(keyNetheriteScrapShard).arch$tab(ModCreativeTab.COELURIACORE_TAB)));
   }
 }
---
+++ common/src/mc_1_21_3/java/io/github/tetratheta/coeluriacore/ModItem.java
@@ -0,0 +1,22 @@
+package io.github.tetratheta.coeluriacore;
+
+import dev.architectury.registry.registries.DeferredRegister;
+import dev.architectury.registry.registries.RegistrySupplier;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+
+@SuppressWarnings("UnstableApiUsage")
+public class ModItem {
+  public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(CoeluriaCore.MOD_ID, Registries.ITEM);
+
+  public static RegistrySupplier<Item> NETHERITE_SCRAP_SHARD;
+
+  static {
+    ResourceLocation idNetheriteScrapShard = ResourceLocation.fromNamespaceAndPath("coeluriacore", "netherite_scrap_shard");
+    ResourceKey<Item> keyNetheriteScrapShard = ResourceKey.create(Registries.ITEM, idNetheriteScrapShard);
+
+    NETHERITE_SCRAP_SHARD = ITEMS.register(idNetheriteScrapShard, () -> new Item(new Item.Properties().setId(keyNetheriteScrapShard).arch$tab(ModCreativeTab.COELURIACORE_TAB)));
+  }
+}
